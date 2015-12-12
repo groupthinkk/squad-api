6d2cd451fe1154be4474e6c86bf612e13fcd1e1c
Bassem Ali <bassem.ali@gmail.com>
2015-12-12 00:52:19 -0500

    Add django rest swagger

--- squadapi/squadapi/urls.py
+++ squadapi/squadapi/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^api/v0/', include('instagram.urls')),
     url(r'^api/v0/', include('predictions.urls')),
+    url(r'^api/docs/', include('rest_framework_swagger.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
