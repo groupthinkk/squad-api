4d4a8bf585e5fe7d35b85687573106baf5ac0cfa
Bassem Ali <bassem.ali@gmail.com>
2015-12-12 00:56:10 -0500

    Commit install.sh

---
+++ install.sh
@@ -0,0 +1,31 @@
+apt-get update
+apt-get install -y python3-pip rabbitmq-server
+apt-get install -y libpq-dev postgresql-client
+sudo chown -R ubuntu:ubuntu /opt/
+
+mkdir /opt/virtualenvs
+cp -r /home/ubuntu/squad-api /opt/
+
+pip3 install virtualenvwrapper
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+export WORKON_HOME=/opt/virtualenvs/
+source /usr/local/bin/virtualenvwrapper.sh 
+cd /opt/squad-api && mkvirtualenv squad-api && workon squad-api
+workon squad-api
+pip3 install -r requirements.txt
+
+mkdir /opt/uwsgi
+sudo chown -R ubuntu:ubuntu /opt/
+
+workon squad-api
+cd /opt/squad-api/squadapi
+export DJANGO_SETTINGS_MODULE=squadapi.production
+python manage.py migrate
+python manage.py loaddata users.json
+python manage.py collectstatic --noinput
+uwsgi --stop /tmp/squadapi-master.pid
+uwsgi --ini /opt/squad-api/uwsgi-production.ini 
+
+apt-get install -y nginx
+ln -sf /opt/squad-api/nginx-squadapi-production.conf /etc/nginx/sites-enabled/default
+service nginx restart
